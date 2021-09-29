b4068c41d7b37d30a56b31e8ecf6d9e7edb3ce4e
Nick Wibert <nickwibert@gmail.com>
2021-09-28 22:42:28 -0400

    Update PM-kmeans.R
    Update with actual k-means clustering

--- PM-kmeans.R
+++ PM-kmeans.R
@@ -3,37 +3,47 @@
 # Last Modified: 09/25/21
 
 
-
 # Libraries ---------------------------------------------------------------
 
-library(ncdf4)
 library(raster)
-library(ecbtools) # from GitHub, function "raster.kmeans()"
-library(cluster)
 
 
 # K-means clustering ------------------------------------------------------
 
-data_dir <- "/Users/nicholasw30/Downloads/PM-kmeans/data"
+data_dir <- "C:/Users/nickw/OneDrive/Desktop/UF/Undergrad Research/PM-research/data/Annual"
 
 setwd(data_dir)
 
-files <- list.files(pattern=".nc")
+names <- c("BC", "NH4", "NIT", "OM", "SO4", "SOIL", "SS")
+files <- c()
+
+# get annual data for all components in the year 2000
+for (name in names)
+{
+  files <- append(files,
+                  file.path(name, 
+                            list.files(path = file.path(data_dir, name),
+                                       pattern="200012.nc")))
+}
 
 r <- stack(files)
 
-# take only 1% of the data
-r.sample <- sampleRandom(r, size=(dim(r)[1]*dim(r)[2]*0.01), xy=TRUE, na.rm=TRUE)
+# crop raster layers to bounded box of USA
+e <- as(extent(-124.848974, -66.885444, 24.396308, 49.384358), 'SpatialPolygons')
+crs(e) <- "+proj=longlat +datum=WGS84 +no_defs"
+usa <- crop(r, e)
 
-xy <- r.sample[,1:2]
+# take a random sample of only 1% of the data
+usa.sample <- as.data.frame(sampleRandom(usa, size=(dim(usa)[1]*dim(usa)[2]*0.01),
+                                         xy=TRUE, na.rm=TRUE))
 
-r.sample <- as.data.frame(r.sample)
+# perform k-means clustering, store cluster id in data frame
+usa.sample <- data.frame(usa.sample, k = kmeans(usa.sample, 7)$cluster)
 
-r.sample <- SpatialPointsDataFrame(coords = xy, data = r.sample,
-                               proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+# plot data points where color is determined by cluster
+plot(usa.sample$x, usa.sample$y, col = usa.sample$k.2,
+     xlab = "Latitude", ylab = "Longitude",
+     main = "K-Means Clustering for 2000 (using all components)",
+     cex.main=0.8, cex.lab = 0.7, cex.axis = 0.6)
 
-
-r.sample@data <- data.frame(r.sample@data, k = clara(scale(r.sample@data), k=5)$clustering)
-
-plot(r.sample$x, r.sample$y, col=r.sample$k)
 
